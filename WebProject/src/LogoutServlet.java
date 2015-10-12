415b1e3eee50bdc88a85f326a4ba44eddd24a928
Poonam Patare <poonam.patare@weboniselab.com>
2015-10-12 15:31:38 +0530

    java assignment changes added

--- WebProject/src/LogoutServlet.java
+++ WebProject/src/LogoutServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 public class LogoutServlet extends HttpServlet {
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			response.setContentType("text/html");
--- WebProject/src/VideoServlet.java
+++ WebProject/src/VideoServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 public class VideoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
